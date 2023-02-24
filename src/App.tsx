793f34fb7e5331dcc356ea4ce10ac8f40bdf368f
Kevin Duliepre <kevinduliepre@gmail.com>
2023-02-24 15:31:04 -0500

    Added Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Rating } from "./components/Rating";
 
 function App() {
   return (
     <div className="App">
       <Rating />
+      <Analytics />
     </div>
   );
 }
